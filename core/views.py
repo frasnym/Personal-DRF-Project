f2ed776197e36cf46a5e240cbca71b5cc08b73d7
frasnym <frastyawan.nym@gmail.com>
2020-11-05 14:02:10 +0800

    Add Description to Code

--- core/views.py
+++ core/views.py
@@ -11,27 +11,31 @@ class NewUserRegistration(viewsets.ViewSet):
     """
     New User Registration
     """
-    
-    renderer_classes = [JSONRenderer] # Class Scope Renderer: Use for generate custom Response message
+    # ? Class Scope Renderer: Use for generate custom Response message
+    renderer_classes = [JSONRenderer] 
 
     def create(self, request):
-        content = {
+        # ? Declare Response Message
+        respMessage = {
             "success": False,
             "message": None,
         }
+        # ? Set Serializer
         serializer = UserRegistrationSerializer(data=request.data)
+
         if serializer.is_valid():
             # TODO check username does not contain space
             # TODO multi language response
             # TODO one way encrypt password 
             # serializer.save()
-            content["message"] = 'Process Success'
-            content["data"] = {
+            respMessage["success"] = True
+            respMessage["message"] = 'Process Success'
+            respMessage["data"] = {
                 'username': serializer.data['username'],
                 'email_address': serializer.data['email_address'],
                 'phone_number': serializer.data['phone_number'],
             }
-            return Response(content, status=status.HTTP_201_CREATED)
+            return Response(respMessage, status=status.HTTP_201_CREATED)
 
-        content["message"] = f'{list(serializer.errors.values())[0][0]}: {list(serializer.errors.keys())[0]}'
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        respMessage["message"] = f'{list(serializer.errors.values())[0][0]}: {list(serializer.errors.keys())[0]}'
+        return Response(respMessage, status=status.HTTP_400_BAD_REQUEST)
