6a013252bc4d4ae34091926a063217e3e9f4fb95
frasnym <frastyawan.nym@gmail.com>
2020-11-06 09:52:37 +0800

    Custom Error Message in Serializer

--- core/serializers.py
+++ core/serializers.py
@@ -3,6 +3,15 @@ from .models import UserRegistration
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        """
+        Custom error message
+        """
+        super(UserRegistrationSerializer, self).__init__(*args, **kwargs) # call the super() 
+        for field in self.fields: # iterate over the serializer fields
+            self.fields[field].error_messages['required'] = '%s field is required'%field # set the custom error message
+            self.fields[field].error_messages['blank'] = '%s field cannot be blank'%field
+    
     def validate(self, data):
         """
         Check if username is contain space
